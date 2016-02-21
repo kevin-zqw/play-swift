fd582aad4f51c779943b29f2974b2550fefa1d80
Kevin Zhang <kevin.zqw@gmail.com>
2016-02-21 23:19:47 +0800

    Error Handling
    Swift’s Exception

--- swift-lang/01e1. Error Handling.playground/Contents.swift
+++ swift-lang/01e1. Error Handling.playground/Contents.swift
@@ -2,4 +2,22 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// Use Error Handling to respond to error conditions your program may encounter during execution.
+// When a function encounters an error condition, it throws an error. That function's caller can then 
+// catch the error and respond appropriately.
+
+func canThrowAnError() throws {
+    // this function may or may not throw an error
+}
+
+do {
+    try canThrowAnError()
+    // no error was thrown
+//} catch Error.SomeError {
+//    
+//} catch Error.AnotherError {
+//    
+} catch {
+    // an error was thrown
+}
+
