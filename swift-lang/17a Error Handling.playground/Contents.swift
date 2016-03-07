53055b626ed62bd78be47cf79f7c92887ad777c6
Kevin Zhang <kevin.zqw@gmail.com>
2016-03-07 17:19:33 +0800

    Error Handling

--- swift-lang/17a Error Handling.playground/Contents.swift
+++ swift-lang/17a Error Handling.playground/Contents.swift
@@ -8,3 +8,20 @@ import UIKit
 // Enum is well suited for Error type
 
 // Error Type can be throwed, and can be catched by caller
+
+enum VendingMachineError: ErrorType {
+    case InvalidSelection
+    case InsufficinetFunds(coinsNeeded: Int)
+    case OutOfStock
+}
+
+func buyStock() throws {
+    throw VendingMachineError.InsufficinetFunds(coinsNeeded: 5)
+}
+
+try buyStock()
+
+// throws go before the return type
+func canThrowErrors() throws -> Int {
+    return 5;
+}
