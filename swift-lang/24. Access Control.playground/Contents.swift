fb8359971908b856d9d6d9b96a3e7f92a3d3a09f
Kevin Zhang <kevin.zqw@gmail.com>
2016-03-15 23:05:43 +0800

    Access Control

--- swift-lang/24. Access Control.playground/Contents.swift
+++ swift-lang/24. Access Control.playground/Contents.swift
@@ -1,5 +1,31 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import Foundation
 
-var str = "Hello, playground"
+// Modules and Source Files, A module is a single unit of code distribution(a Framework or Application)
+// Swift's access control is based on Modules and Source Files
+
+// Public access
+// Internal access
+// Private access
+
+// Guiding Principle of Access Levels
+// No entity can be defined in terms of another entity that has a lower(more restrictive) access level.
+
+// Access level for Single-Target Apps is: internal
+// Access level for Framework is: Mark Public to make it can be viewed and accessed by other modules.
+// Access level for Unit Test Targets: Use @testable attribute and compile that product module 
+// with testing enabled.
+
+public class SomePublicClass {}
+internal class SomeInternalCalss {}
+private class SomePrivateClass {}
+
+public var publicVar = 0
+internal let internalVar = 5
+
+private func somePrivateFunction() {}
+
+// The default access level is internal
+
