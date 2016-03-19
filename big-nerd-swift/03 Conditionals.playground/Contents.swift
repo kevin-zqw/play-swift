be0b519a13de479868f3b25ccc57b362791904f8
Kevin Zhang <kevin.zqw@gmail.com>
2016-03-19 22:31:06 +0800

    Conditionals

---
+++ big-nerd-swift/03 Conditionals.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let number = 5
+
+if number == 5 {
+    print("number is \(number)")
+} else {
+    print("number is not \(number)")
+}
+
+// === tells whether the two instances point to the same reference
+// !== not the same references
+
+// If statements only allow boolean value
+// You can omit the () around testing case
+// You must always provide {} for if, even if you have only one line code
+
+// Ternary Operator
+// someBool ? trueStatement : falseStatement
+
